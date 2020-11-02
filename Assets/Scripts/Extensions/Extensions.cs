840c1e2b352c07d5b8c8faa44a1d468ce21b6c09
Wesley Raymundo <wesley.nasray@petitfabrik.com>
2020-11-02 15:15:26 -0300

    Added LayerMask extension;

---
+++ Assets/Scripts/Extensions/Extensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class LayerMaskExtensions
+{
+    public static bool Includes(this LayerMask layerMask, int layer)
+    {
+        return layerMask == (layerMask.value | (1 << layer));
+    }
+}
