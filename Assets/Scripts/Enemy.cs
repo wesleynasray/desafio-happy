2c35d88665ee5b18d46c20f3b23d084a382cb851
Wesley Raymundo <wesley.nasray@petitfabrik.com>
2020-11-02 14:40:01 -0300

    Target nearest enemy on update;

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public static List<Enemy> Enemies = new List<Enemy>();
+
+    private void OnEnable()
+    {
+        Enemies.Add(this);
+    }
+
+    private void OnDisable()
+    {
+        Enemies.Remove(this);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -10,12 +11,38 @@ public class Player : MonoBehaviour
     CharacterController m_Controller;
     Vector3 m_MoveInput;
 
+    Transform target;
+
     private void Awake()
     {
         m_Controller = GetComponent<CharacterController>();
     }
 
     private void Update()
+    {
+        Move();
+        TargetClosestEnemy();
+    }
+
+    private void TargetClosestEnemy()
+    {        
+        Enemy closestEnemy = null;
+        float closestSqrDistance = float.PositiveInfinity;
+
+        foreach (var enemy in Enemy.Enemies)
+        {
+            var sqrDistance = Vector3.SqrMagnitude(enemy.transform.position - transform.position);
+            if (sqrDistance < closestSqrDistance)
+            {
+                closestSqrDistance = sqrDistance;
+                closestEnemy = enemy;
+            }
+        }
+
+        target = closestEnemy.transform;
+    }
+
+    private void Move()
     {
         m_Controller.Move(m_MoveInput.normalized * m_MoveSpeed * Time.deltaTime);
     }
