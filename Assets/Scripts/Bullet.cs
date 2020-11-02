75a1dd4b35b39e6df999d640f4a951a357de1f29
Wesley Raymundo <wesley.nasray@petitfabrik.com>
2020-11-02 15:15:51 -0300

    Added Bullet prefab and script;

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] LayerMask collisionMask;
+
+    private void Update()
+    {
+        transform.Translate(transform.forward * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(collisionMask.Includes(other.gameObject.layer))
+            Destroy(gameObject);
+    }
+}
