de361168b52ea30a017c08b7e6d37ec6cac00571
Wesley Raymundo <wesley.nasray@petitfabrik.com>
2020-11-04 09:23:55 -0300

    Show power up modal on wave end; Start next wave on power up option click;Added buttom scaler component;

---
+++ Assets/Scripts/UI/Modals/PowerUpModal.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpModal : MonoBehaviour
+{
+    [SerializeField] GameObject content;
+    
+    private void OnEnable() => SpawnArea.OnSpawnFinish += SpawnArea_OnSpawnFinish;
+    private void OnDisable() => SpawnArea.OnSpawnFinish += SpawnArea_OnSpawnFinish;
+
+    private void SpawnArea_OnSpawnFinish(SpawnArea spawnArea) => content.SetActive(true);
+}
---
+++ Assets/Scripts/UI/PowerUpOption.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PowerUpOption : MonoBehaviour, IPointerClickHandler
+{
+    public static event Action<PowerUpOption> OnClicked;
+    public void OnPointerClick(PointerEventData eventData) => OnClicked?.Invoke(this);
+}
