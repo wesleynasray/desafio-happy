7202b6ec8305fdbab1ea832e8e4def6d24b9d0f5
Wesley Raymundo <wesley.nasray@petitfabrik.com>
2020-11-04 07:19:56 -0300

    Show game over modal when player dies;

---
+++ Assets/Scripts/UI/Modals/GameOverModal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverModal : MonoBehaviour
+{
+    [SerializeField] Damageable player;
+    [Space]
+    [SerializeField] GameObject content;
+    [SerializeField] Text scoreCounter;
+    
+    private void OnEnable() => player.OnDeath += Player_OnDeath;
+    private void OnDisable() => player.OnDeath -= Player_OnDeath;
+
+    private void Player_OnDeath()
+    {
+        scoreCounter.text = Score.Amount.ToString();
+        content.SetActive(true);
+    }
+
+    public void PlayAgainClicked()
+    {
+        // TODO: send highscore
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
