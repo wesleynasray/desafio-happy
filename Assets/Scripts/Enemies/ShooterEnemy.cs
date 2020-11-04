660f681a2b02a0df59a1a69e13bd239e3484b91f
Wesley Raymundo <wesley.nasray@petitfabrik.com>
2020-11-04 14:22:12 -0300

    Added shooter enemy script;

---
+++ Assets/Scripts/Enemies/ShooterEnemy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterEnemy : Enemy
+{
+    [SerializeField] LookTowards look;
+
+    private void Start()
+    {
+        look.target = FindObjectOfType<Player>().transform;
+    }
+}
