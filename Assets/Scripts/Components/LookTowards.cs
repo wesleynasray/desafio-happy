18cd91b9bbc0c75244900d257e05a5c7e8afaa7e
Wesley Raymundo <wesley.nasray@petitfabrik.com>
2020-11-04 14:22:00 -0300

    Added lookTowards component;

---
+++ Assets/Scripts/Components/LookTowards.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookTowards : MonoBehaviour
+{
+    public Transform target;
+
+    private void Update()
+    {
+        if(target != null)
+            transform.LookAt(target);
+    }
+}
