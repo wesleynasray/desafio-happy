f1972643657aaf140f1eb536f1ab2960b0b0d215
Wesley Raymundo <wesley.nasray@petitfabrik.com>
2020-11-04 14:30:14 -0300

    Avoid ShooterEnemy shooting without target;

--- Assets/Scripts/Enemies/ShooterEnemy.cs
+++ Assets/Scripts/Enemies/ShooterEnemy.cs
@@ -6,9 +6,20 @@ using UnityEngine;
 public class ShooterEnemy : Enemy
 {
     [SerializeField] LookTowards look;
+    [SerializeField] Shooter shooter;
+    [SerializeField] float startDelay;
 
-    private void Start()
+    protected override void Awake()
+    {
+        base.Awake();
+        shooter.enabled = false;
+    }
+
+    private IEnumerator Start()
     {
         look.target = FindObjectOfType<Player>().transform;
+        
+        yield return new WaitForSeconds(startDelay);
+        shooter.enabled = true;
     }
 }
