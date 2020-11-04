1a5fa81ef241311394e6034de790ec0c13899d2e
Wesley Raymundo <wesley.nasray@petitfabrik.com>
2020-11-04 10:07:59 -0300

    Added scriptable objects for directional bullet power ups

---
+++ Assets/Scripts/PowerUps/BulletPowerUp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BulletPowerUp", menuName = "ScriptableObjects/BulletPowerUp")]
+public class BulletPowerUp : PowerUpBase
+{
+    [Header("Bullet")]
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] BulletSetup[] bulletSetups;
+
+    [System.Serializable]
+    public struct BulletSetup
+    {
+        public Vector3 direction;
+        public Vector3 offset;
+    }
+
+    public override void Action(GameObject caller)
+    {
+        foreach(var setup in bulletSetups)
+        {
+            Instantiate(
+                bulletPrefab,
+                caller.transform.position + setup.offset, 
+                Quaternion.Euler(caller.transform.TransformDirection(setup.direction.normalized))
+            );
+        }
+    }
+}
---
+++ Assets/Scripts/PowerUps/PowerUpBase.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PowerUpBase : ScriptableObject
+{
+    [SerializeField] Sprite icon;
+    [SerializeField] PowerUpBase[] powerUpsToOverride;
+    public abstract void Action(GameObject caller);
+}
